727100e5656feb86a855ab42ebf6571b86a7f47a
Neeraj-C08 <neerajchandekar@gmail.com>
2024-09-03 15:19:50 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,9 +1,47 @@
-//import stuff here
+//import stuff here//Your code here
+import java.util.Scanner;
 
-//Your code here
+public class program7 {
+    
+public static void main(String[] args){
+    
+    double sb;
+    
+    double klevins;
+    
+    double sn;
+    
+    double newSb;
+    
+    Scanner newNum = new Scanner(System.in);
+    
+    System.out.print("Enter number of schrute-bucks: ");
+    
+    sb = newNum.nextDouble();
+    
+    System.out.print("Enter number of klevins: ");
+    
+    klevins = newNum.nextDouble();
+    
+    System.out.print("Enter number of stanley-nickels");
+    
+    sn = newNum.nextDouble();
+    
+   newSb = sb + klevins*(1.0/20) + sn*(1.0/240);
+   
+   System.out.println(newSb);
+}
+
+
+
+}
+
 
 //Paste console output below:
 /*
-
+Enter number of schrute-bucks: 7
+Enter number of klevins: 17
+Enter number of stanley-nickels9
+7.887499999999999
 
 */
